045763f97cea40f8161e65c8bf99190d9001204e
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-20 18:16:02 +0600

    adding sort method

--- src/components/Home/Project.jsx
+++ src/components/Home/Project.jsx
@@ -1,38 +1,39 @@
 import "./Project.css"
 import project from "../../../public/projects.json"
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 const Project = () => {
-    // console.log(project);
 
+    const [array , setArray] = useState(project)
     useEffect(()=>{
         AOS.init()
     },[])
+    console.log(array);
 
     return (
         <section data-aos="fade-down"
         // data-aos-offset="500"
         // data-aos-easing="ease-out-cubic"
         data-aos-duration="2000"
-            className="bg-black h-full">
+            className="bg-black lg:h-screen h-full">
             <div>
                 <span id="projects" className="lg:text-8xl md:text-7xl text-6xl text-gray-800 opacity-60 font-extrabold absolute ">Projects</span>
                 <h1 className="lg:text-6xl md:text-5xl text-4xl text-center relative text-white font-bold lg:top-5 md:top-4 top-3">My <span className="text-blue-800">Projects</span></h1>
             </div>
             <div className="py-10">
-                <div className=" flex justify-end mr-5">
-                <select className="text-xl" name="" id="">
+                <div className=" flex justify-end ">
+                <select className="text-xl p-1 mr-10 rounded-lg" name="" id="">
                 <option value="default">Default</option>
                 <option value="newest">Newest</option>
                 <option value="oldest">Oldest</option>
                 </select>
                 </div>
                 <div>
-                <div className="flex  flex-wrap gap-5 justify-center lg:my-10 my-5">
+                <div className="flex  flex-wrap gap-5 justify-center lg:my-8 my-5">
                     {
-                        project.map((element,idx)=> <Card key={idx} id={idx} card={element} ></Card>)
+                        array.map((element,idx)=> <Card key={idx} id={idx} card={element} ></Card>)
                     }
                 </div>
                 </div>
